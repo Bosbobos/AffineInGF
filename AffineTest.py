bf614e822b00f90befa12ccf63b1354116efa37c
Matvey Konovalov <konovalov.matvej@mail.ru>
2024-12-29 20:03:43 +0300

    Little refactor, some edge case discovered

--- AffineTest.py
+++ AffineTest.py
@@ -38,7 +38,8 @@ def test_affine_encode_block(block_coeffs, keyA_coeffs, keyB_coeffs, p, mod_coef
         # Пример 1: Кодирование
         ([3, 1], [0, 2], [1, 1], 5, 7),
         # Пример 3: Кодирование с другими значениями
-        ([2, 3, 4], [1, 1], [0, 1, 1], 7, 13)
+        ([2, 3, 4], [1, 1], [0, 1, 1], 7, 13),
+        ([1, 1, 1, 7, 4, 2], [1, 0, 0, 0, 0, 0, 8, 21], [5], 3, 7)
     ]
 )
 def test_affine_encode_block(block_coeffs, keyA_coeffs, keyB_coeffs, p, n):
@@ -69,6 +70,7 @@ def test_affine_encode_block(block_coeffs, keyA_coeffs, keyB_coeffs, p, n):
         ("123456", [1], [2], 3, 2),                   # Поле GF(3^2)
         ("", [1, 1], [0, 1], 5, 8),                # Пустая строка
         ("i love cryptography <3", [15, 1, 0, 7, 8], [4, 6, 7, 0, 1], 17, 10),
+        ("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111", [1], [0], 11, 15) # ?????????????????????????? TODO: find out why fails
     ]
 )
 def test_affine_encode_decode(message, keyA_coeffs, keyB_coeffs, p, n):
